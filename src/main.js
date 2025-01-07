a19b5718a859172af715f6837d68a7e1121d5e7a
MRBrath93 <147531334+MRBrath93@users.noreply.github.com>
2025-01-07 21:21:33 +0100

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './assets/vueMain.css'; // CSS-styling
+import { createApp } from 'vue'; // Vue-applikationen
+import { createPinia } from 'pinia'; // State management
+import App from './App.vue'; // Hovedapplikationskomponenten
+import router from './router'; // Importer Vue Router
+
+const app = createApp(App);
+
+app.use(createPinia()); // Bruger Pinia som state management
+app.use(router); // Bruger Vue Router til navigation
+
+app.mount('#app'); // Monterer appen til DOM-elementet med id 'app'
