40da5712bb15a9781be7c0889ef8763457cf8138
MRBrath93 <147531334+MRBrath93@users.noreply.github.com>
2025-01-07 23:06:54 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import ProductAllGlassesOverview from '@/views/ProductAllGlassesOverview.vue';
 import ProductDetails from '@/views/ProductDetails.vue';
 import ProductMenOverview from '@/views/ProductMenOverview.vue';
@@ -70,7 +70,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
